745e9ad55bc5c51d9482fd6bab03d57d3f618996
Arman <thisiisarman@gmail.com>
2016-07-25 00:12:18 +0430

    Add files via upload

--- plugins/dnup.lua
+++ plugins/dnup.lua
@@ -3,58 +3,52 @@ local function run(msg, matches)
   local url = matches[1]
   local ext = matches[2]
   local file = download_to_file(url)
+ local size = gethttpsize(matches[1])
   local cb_extra = {file_path=file}
-  local dick = 'File Was Uploaded To Our Server\n\nSending To Telegram May Takes Some Minutes...\nCheck Out Our Rules in \n https://telegram.me/joinchat/BhviQz87RSFvOI2O00thOw\n\n@UltraKing ❤️'
+  local dick =  'File Was Uploaded To Our Server\nSending To Telegram May Takes Some Minutes...\nSome Info:\nSize: '..nicesize(tonumber(size))..'\nType: '..ext..'\nURL: '..url
   local mime_type = mimetype.get_content_type_no_sub(ext)
   if ext == 'gif' then
+  
     print('send_file')
+			send_msg(receiver, dick, ok_cb, nil)
     send_document(receiver, file, rmtmp_cb, cb_extra)
-		send_msg(receiver, dick, ok_cb, nil)
 
   elseif mime_type == 'text' then
     print('send_document')
+			send_msg(receiver, dick, ok_cb, nil)
     send_document(receiver, file, rmtmp_cb, cb_extra)
-  		send_msg(receiver, dick, ok_cb, nil)
 
   elseif mime_type == 'image' then
     print('send_photo')
+			send_msg(receiver, dick, ok_cb, nil)
     send_photo(receiver, file, rmtmp_cb, cb_extra)
-  		send_msg(receiver, dick, ok_cb, nil)
 
   elseif mime_type == 'audio' then
     print('send_audio')
+			send_msg(receiver, dick, ok_cb, nil)
     send_audio(receiver, file, rmtmp_cb, cb_extra)
-  		send_msg(receiver, dick, ok_cb, nil)
 
   elseif mime_type == 'video' then
     print('send_video')
+			send_msg(receiver, dick, ok_cb, nil)
     send_video(receiver, file, rmtmp_cb, cb_extra)
-	send_msg(receiver, dick, ok_cb, nil)
 
   else
     print('send_file')
+				send_msg(receiver, other, ok_cb, nil)
+		send_msg(receiver, dick, ok_cb, nil)
     send_file(receiver, file, rmtmp_cb, cb_extra)
-	send_msg(receiver, dick, ok_cb, nil)
+
 end
 end
 return {
   description = "When user sends media URL (ends with gif, mp4, pdf, etc.) download and send it to origin.", 
   usage = "",
   patterns = {
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(gif))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(mp4))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(pdf))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(ogg))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(zip))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(mp3))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(rar))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(wmv))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(doc))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(avi))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(webp))$",
-	"(https?://[%w-_%.%?%.:/%+=&]+%.(exe))$",
-	"(https?://[%w-_%.%?%.:/%+=&]+%.(apk))$",
-	"(https?://[%w-_%.%?%.:/%+=&]+%.(mkv)(.*))$"
+    "(https?://[%w-_%.%?%.:/%+=&]+%.(.*))$",
+	
+
+	
   }, 
   run = run 
 }
